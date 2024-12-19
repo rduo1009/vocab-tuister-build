44174d003d2a70f1ae87d3d1866c1e293636e0d2
rduo1009 <robertduong09alt@gmail.com>
2024-12-19 16:55:37 +0700

    ✨ feat: (wip-cont) using environment variables

--- macos/scripts/create_stdlib_mac.py
+++ macos/scripts/create_stdlib_mac.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 
@@ -6,14 +7,30 @@ if sys.platform != "darwin":
         f"This script should only be run on macOS (got {sys.platform})"
     )
 
+PYTHON_NAME = os.environ["PYTHON_NAME"]
+PYTHON_VERSION_SSHORT = os.environ["PYTHON_VERSION_SSHORT"]
+PYTHON_VERSION_SHORT = os.environ["PYTHON_VERSION_SHORT"]
+PYTHON_VERSION_LONG = os.environ["PYTHON_VERSION_LONG"]
+BREW_PYTHON_FORMULA = os.environ["BREW_PYTHON_FORMULA"]
+
 # fmt: off
 STDLIB_EXTENSIONS = ["_asyncio", "_bisect", "_blake2", "_bz2", "_codecs_cn", "_codecs_hk", "_codecs_iso2022", "_codecs_jp", "_codecs_kr", "_codecs_tw", "_contextvars", "_csv", "_ctypes", "_curses", "_datetime", "_dbm", "_decimal", "_elementtree", "_hashlib", "_heapq", "_json", "_lzma", "_md5", "_multibytecodec", "_multiprocessing", "_opcode", "_pickle", "_posixshmem", "_posixsubprocess", "_queue", "_random", "_scproxy", "_sha1", "_sha2", "_sha3", "_socket", "_sqlite3", "_ssl", "_statistics", "_struct", "_uuid", "array", "binascii", "fcntl", "grp", "math", "mmap", "pyexpat", "readline", "resource", "select", "syslog", "termios", "unicodedata", "zlib"]
 # fmt: on
 
 for extension in STDLIB_EXTENSIONS:
-    arm64_binary = f"/opt/homebrew/opt/python@3.13/Frameworks/Python.framework/Versions/3.13/lib/python3.13/lib-dynload/{extension}.cpython-313-darwin.so"
-    x86_binary = f"/usr/local/opt/python@3.13/Frameworks/Python.framework/Versions/3.13/lib/python3.13/lib-dynload/{extension}.cpython-313-darwin.so"
-    output_binary = f"macos/stdlib/{extension}.cpython-313-darwin.so"
+    arm64_binary = (
+        f"/opt/homebrew/opt/{BREW_PYTHON_FORMULA}/Frameworks/Python.framework/"
+        f"Versions/{PYTHON_VERSION_SHORT}/lib/{PYTHON_NAME}/lib-dynload/"
+        f"{extension}.cpython-{PYTHON_VERSION_SSHORT}-darwin.so"
+    )
+    x86_binary = (
+        f"/usr/local/opt/{BREW_PYTHON_FORMULA}/Frameworks/Python.framework/"
+        f"Versions/{PYTHON_VERSION_SHORT}/lib/{PYTHON_NAME}/lib-dynload/"
+        f"{extension}.cpython-{PYTHON_VERSION_SSHORT}-darwin.so"
+    )
+    output_binary = (
+        f"macos/stdlib/{extension}.cpython-{PYTHON_VERSION_SSHORT}-darwin.so"
+    )
 
     subprocess.run(
         [
