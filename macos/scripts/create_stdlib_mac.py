45fd0919cf5c53656a3c2f7573c026e8a50a1a3e
rduo1009 <robertduong09alt@gmail.com>
2024-12-19 16:58:41 +0700

    🔨 refactor: list should be tuple

--- macos/scripts/create_stdlib_mac.py
+++ macos/scripts/create_stdlib_mac.py
@@ -14,7 +14,7 @@ PYTHON_VERSION_LONG = os.environ["PYTHON_VERSION_LONG"]
 BREW_PYTHON_FORMULA = os.environ["BREW_PYTHON_FORMULA"]
 
 # fmt: off
-STDLIB_EXTENSIONS = ["_asyncio", "_bisect", "_blake2", "_bz2", "_codecs_cn", "_codecs_hk", "_codecs_iso2022", "_codecs_jp", "_codecs_kr", "_codecs_tw", "_contextvars", "_csv", "_ctypes", "_curses", "_datetime", "_dbm", "_decimal", "_elementtree", "_hashlib", "_heapq", "_json", "_lzma", "_md5", "_multibytecodec", "_multiprocessing", "_opcode", "_pickle", "_posixshmem", "_posixsubprocess", "_queue", "_random", "_scproxy", "_sha1", "_sha2", "_sha3", "_socket", "_sqlite3", "_ssl", "_statistics", "_struct", "_uuid", "array", "binascii", "fcntl", "grp", "math", "mmap", "pyexpat", "readline", "resource", "select", "syslog", "termios", "unicodedata", "zlib"]
+STDLIB_EXTENSIONS = ("_asyncio", "_bisect", "_blake2", "_bz2", "_codecs_cn", "_codecs_hk", "_codecs_iso2022", "_codecs_jp", "_codecs_kr", "_codecs_tw", "_contextvars", "_csv", "_ctypes", "_curses", "_datetime", "_dbm", "_decimal", "_elementtree", "_hashlib", "_heapq", "_json", "_lzma", "_md5", "_multibytecodec", "_multiprocessing", "_opcode", "_pickle", "_posixshmem", "_posixsubprocess", "_queue", "_random", "_scproxy", "_sha1", "_sha2", "_sha3", "_socket", "_sqlite3", "_ssl", "_statistics", "_struct", "_uuid", "array", "binascii", "fcntl", "grp", "math", "mmap", "pyexpat", "readline", "resource", "select", "syslog", "termios", "unicodedata", "zlib")
 # fmt: on
 
 for extension in STDLIB_EXTENSIONS:
