f095e721036b9e36856b698c2ecb5f14002327ba
rduo1009 <robertduong09alt@gmail.com>
2025-10-11 12:54:13 +1100

    🔨 refactor: update to python 3.14

--- macos/scripts/create-wheels-mac.sh
+++ macos/scripts/create-wheels-mac.sh
@@ -37,3 +37,29 @@ done
 } >> logs/macos.log
 
 rm "$x86_64_wheel" "$arm64_wheel"
+
+# ------------------------------------------------------------------------
+# MARKUPSAFE WHEEL
+
+curl -L "$MARKUPSAFE_WHEEL_X86_URL" -O
+curl -L "$MARKUPSAFE_WHEEL_ARM_URL" -O
+
+x86_64_wheel=$(basename "$MARKUPSAFE_WHEEL_X86_URL")
+arm64_wheel=$(basename "$MARKUPSAFE_WHEEL_ARM_URL")
+
+delocate-merge "$x86_64_wheel" "$arm64_wheel" --wheel-dir macos/wheels
+
+wheels=(macos/wheels/*.whl) 
+for w in $wheels; do
+  [[ $w == "$output_wheel" ]] && continue
+  output_wheel=$w
+done
+
+{
+  echo "${arm64_wheel:t}    $(shasum -a 256 "$arm64_wheel" | awk '{ print $1 }')"
+  echo "${x86_64_wheel:t}    $(shasum -a 256 "$x86_64_wheel" | awk '{ print $1 }')"
+  echo "${output_wheel:t}    $(shasum -a 256 "$output_wheel" | awk '{ print $1 }')"
+  echo ""
+} >> logs/macos.log
+
+rm "$x86_64_wheel" "$arm64_wheel"
