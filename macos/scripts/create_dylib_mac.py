e8f40da23cf548d92cbcb25372616a7f2a77cbc5
rduo1009 <robertduong09alt@gmail.com>
2025-10-15 16:27:34 +1100

    🐛 fix: new dylibs for python3.14

--- macos/scripts/create_dylib_mac.py
+++ macos/scripts/create_dylib_mac.py
@@ -8,11 +8,14 @@ from pathlib import Path
 
 log_file_path = Path(__file__).parent.parent.parent / "logs" / "macos.log"
 
-# fmt: off
-PYTHON_DYLIBS = ("libpython3.14", "libssl.3", "libcrypto.3", "libncursesw.5")
-HOMEBREW_DYLIBS = ((("gettext", "libintl.8"), ("xz", "liblzma.5"), ("mpdecimal", "libmpdec.4"), ("libb2", "libb2.1"), ("readline", "libreadline.8"), ("sqlite", "libsqlite3.0")))
-# fmt: on
-
+PYTHON_DYLIBS = ("libpython3.14", "libssl.3", "libcrypto.3", "libzstd.1")
+HOMEBREW_DYLIBS = (
+    ("gettext", "libintl.8"),
+    ("xz", "liblzma.5"),
+    ("mpdecimal", "libmpdec.4"),
+    ("readline", "libreadline.8"),
+    ("zlib", "libz.1")
+)
 
 def _sha256sum(filename: Path):
     with filename.open("rb", buffering=0) as f:
